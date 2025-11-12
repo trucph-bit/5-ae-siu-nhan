500ff66fc0c4cf45d3c8ac66f80b572b2fde2953
trucph <taekookft2@gmail.com>
2025-11-12 22:21:41 +0700

    Window1Ext

--- Window1Ext.py
+++ Window1Ext.py
@@ -28,9 +28,26 @@ class WindowExt(Ui_MainWindow):
         if result == QMessageBox.StandardButton.Ok:
             self.MainWindow.close()
     def total_value(self):
-        pass
+        quantity_moisturiser = int(self.lineEdit_moisturiser.text())
+        quantity_bdywash = int(self.lineEdit_bdywash.text())
+        quantity_cleanser = int(self.lineEdit_cleanser.text())
+        quantity_showergel = int(self.lineEdit_showergel.text())
+        quantity_cream = int(self.lineEdit_cream.text())
+        total_value_calculated = (quantity_moisturiser * 180000) + \
+                                 (quantity_bdywash * 275600) + \
+                                 (quantity_cleanser * 98500) + \
+                                 (quantity_showergel * 254300) + \
+                                 (quantity_cream * 190600)
+        return total_value_calculated
     def Reward(self):
-        pass
+        total_value_calculated=self.total_value()
+        self.Phanthuong.hide()
+        if total_value_calculated >= 1000000:
+            self.label_26.setText("Xin chúc mừng! Bạn đã nhận được bộ serum dưỡng ẩm trị giá 300k")
+            elif total_value_calculated >= 500000:
+                self.label_26.setText("Xin chúc mừng! Bạn đã nhận được set mặt nạ trị giá 119k")
+            elif total_value_calculated >= 100000:
+                self.label_26.setText("Xin chúc mừng! Bạn đã nhận được bông tẩy trang trị giá 39k"))
 
 
 
